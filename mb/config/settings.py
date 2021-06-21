437f043bb05370c2b9a94850c7143b166c68438f
william-cass-wright <william.cass.wright@gmail.com>
2021-06-20 22:11:28 -0700

    heroku deployment

--- mb/config/settings.py
+++ mb/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'onxay$_bs6n9#-g=$b47zq^wo840pwo24yemf-zg^d!741k3l&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
