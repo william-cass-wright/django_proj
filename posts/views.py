02e76aa09ccff27decde0cc4b21d1c5655cd0a19
william-cass-wright <william.cass.wright@gmail.com>
2021-06-20 23:14:39 -0700

    name change

--- posts/views.py
+++ posts/views.py
@@ -6,4 +6,4 @@ from .models import Post
 class HomePageView(ListView):
 	model = Post
 	template_name = 'home.html'
-	context_object_name = 'all_posts_list'
+	# context_object_name = 'all_posts_list'
